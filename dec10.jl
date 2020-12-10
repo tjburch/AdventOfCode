b0bef5283b7de5f402df4372ae37af277e89e5af
Tyler Burch <burcht11@gmail.com>
2020-12-10 09:00:15 -0600

    Add Dec 10, problem 2

--- dec10.jl
+++ dec10.jl
@@ -1,3 +1,5 @@
+## Problem text: https://adventofcode.com/2020/day/10
+
 using StatsBase
 
 f = open("inputs/dec10-input.txt")
@@ -23,3 +25,20 @@ vc = countmap(diffs)
 println("Number of differences of 3: ", vc[3])
 println("Number of differences of 1: ", vc[1])
 println("Multiplied: ", vc[3] * vc[1])
+
+
+combinations = zeros(Int, length(sorted_vals))
+combinations[end] = 1  # know this one will be true a priori
+
+# Count back from the second to last index
+for i in (length(sorted_vals)-1):-1:1
+    lcount = 0
+    for j in 1:3 
+        if i + j <= length(sorted_vals) && 1 <= sorted_vals[i + j] - sorted_vals[i] <= 3
+            lcount += combinations[i+j]
+        end
+    end
+    combinations[i] = lcount    
+end
+
+println("Total number of combinations: ", combinations[1])
