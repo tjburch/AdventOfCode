eddeacd3cff60bff51d37531a5a9edbee9b8f5b0
Tyler Burch <burcht11@gmail.com>
2022-12-01 22:27:30 -0500

    Add dec1 soln

---
+++ 2022/README.md
@@ -0,0 +1,3 @@
+# AdventOfCode-2022
+
+This year I undoubtedly will not have time to do all the advent challenges. As a personal challenge, I'm going to commit to working on each solution I submit for exactly 30 minutes, and will commit whatever I have after that time.
---
+++ 2022/dec1.jl
@@ -0,0 +1,24 @@
+# Day 1:
+# Sucess! Time elapse = 9 minutes
+
+# Text: https://adventofcode.com/2022/day/1
+
+f = open("2022/inputs/dec1-input.txt")
+lines = readlines(f)
+
+total_elfs = sum(lines .== "") + 1
+elfs = zeros(total_elfs)
+i = 1
+for l in lines
+    if l != ""
+        elfs[i] += parse(Int, l)
+    else
+        i += 1
+    end
+end
+
+# Part 1
+elfs |> maximum |> Int |> println
+
+# Part 2
+sort(elfs)[end-2:end] |> sum |> Int |> println
